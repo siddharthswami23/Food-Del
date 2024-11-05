34a4f24dd19f06385df130ee18c50a6e3a4f447e
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-05 18:47:12 +0530

    app download

--- frontend/src/components/AppDownload.jsx
+++ frontend/src/components/AppDownload.jsx
@@ -3,11 +3,21 @@ import { assets } from "../assets/images/assets";
 
 const AppDownload = () => {
   return (
-    <div>
-      <h1>For Better Experience Download Tomato App</h1>
-      <div className="">
-        <img src={assets.app_store} alt="" />
-        <img src={assets.play_store} alt="" />
+    <div className="w-full flex flex-col items-center justify-center py-16 ">
+      <h1 className="text-6xl text-center font-regular leading-relaxed mb-8">
+        For Better Experience Download <br /> Tomato App
+      </h1>
+      <div className="w-full flex justify-center gap-[100px]">
+        <img
+          src={assets.app_store}
+          alt="App Store"
+          className="w-48 h-auto cursor-pointer rounded-3xl duration-300 scale-125 hover:scale-150 border-solid border-[2px] border-white "
+        />
+        <img
+          src={assets.play_store}
+          alt="Play Store"
+          className="w-48 h-auto cursor-pointer rounded-3xl duration-300 scale-125 hover:scale-150 border-solid border-[2px] border-white "
+        />
       </div>
     </div>
   );
