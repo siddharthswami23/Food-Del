491379cb992de01ae7b7caff9438de73e23d6f10
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-05 13:35:32 +0530

    browserrouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
     </div>
   );
 };
