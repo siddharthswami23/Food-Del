bb1f402ee3571b5dab671014528f7cf66a41536f
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-06 12:00:57 +0530

    food display filter

--- frontend/src/components/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay.jsx
@@ -6,16 +6,17 @@ const FoodDisplay = ({ Category, setCategory }) => {
   const { food_list } = useContext(StoreContext);
 
   return (
-    <div className="mx-auto max-w-[75vw] py-10 px-3  rounded-lg shadow-lg">
+    <div className="mx-auto max-w-[75vw] py-10 px-3 rounded-lg shadow-lg">
       <h1 className="font-bold text-7xl mb-10">Top dishes near you</h1>
       <div className="flex gap-5 flex-wrap flex-shrink-0">
-        {food_list.map((item, index) => (
-          <FoodItem key={index} item={item} />
-        ))}
+        {food_list
+          .filter(item => item.category === Category || Category === "All")
+          .map((item, index) => (
+            <FoodItem key={index} item={item} />
+          ))}
       </div>
     </div>
   );
 };
-
 
 export default FoodDisplay;
