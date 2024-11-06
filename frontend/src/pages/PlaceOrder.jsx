2e1b2b0becd43b005f172d789dcf4f6e42498845
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-07 00:22:23 +0530

    del info

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -7,8 +7,10 @@ const PlaceOrder = () => {
   const { getTotalAmount } = useContext(StoreContext);
   return (
     <>
-      <div className="text-white w-full min-h-fit bg-gray-900 p-10 pb-20">
-        <p className="text-2xl font-bold text-center py-5">Delivery Information</p>
+      <div className="text-white w-full min-h-fit p-10 pb-20">
+        <p className="text-2xl font-bold text-left ml-[13.5%] py-5">
+          Delivery Information
+        </p>
         <div className="mx-auto max-w-[75%] flex flex-col md:flex-row items-start md:items-center text-white justify-between p-5 space-y-5 md:space-y-0 md:space-x-5">
           <div className="w-full md:w-1/2 flex flex-col space-y-4">
             <div className="flex space-x-4">
@@ -82,21 +84,31 @@ const PlaceOrder = () => {
             />
           </div>
 
-          <div className=" w-fit  p-10 bg-gray-800 rounded-lg">
-            <h1 className="text-xl font-bold mb-5">
-              If You Have a Promo Code, Enter It Here
-            </h1>
-
-            <div className="flex w-full justify-between space-x-4">
-              <input
-                type="text"
-                className="w-3/4 p-4 text-xl bg-gray-700 border border-gray-600 rounded"
-                placeholder="Enter Promo Code"
-              />
-              <button className="w-1/4 p-2 bg-orange-500 hover:bg-orange-600 rounded">
-                Apply
-              </button>
+          <div className="w-[25vw]  p-10 capitalize">
+            <h1 className="text-5xl font-bold capitalize mb-5">card total</h1>
+            <div className="flex w-full justify-between py-5 ">
+              <p className="text-3xl">Subtotal</p>
+              <p>$ {getTotalAmount()}</p>
             </div>
+            <hr />
+            <div className="flex w-full justify-between py-5">
+              <p className="text-3xl">Delivery fee</p>
+              <p>${getTotalAmount() === 0 ? "0" : "2"}</p>
+            </div>
+            <hr />
+            <div className="flex w-full justify-between py-5">
+              <p className="text-3xl">Total</p>
+              <p>${getTotalAmount() === 0 ? "0" : getTotalAmount() + 2}</p>
+            </div>
+
+            <button
+              onClick={() => {
+                navigate("/order");
+              }}
+              className="w-[70%] p-3 capitalize text-xl font-bold mt-5 rounded-lg bg-orange-500 hover:bg-orange-600"
+            >
+              proceed to payment
+            </button>
           </div>
         </div>
       </div>
