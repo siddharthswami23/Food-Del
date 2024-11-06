90346edd95be2804a10ef3634a276b70b1b1c707
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-06 17:30:51 +0530

    link navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,9 +7,11 @@ const Navbar = ({ setShowLogin }) => {
 
   return (
     <div className="mx-10 max-w-full lg:mx-auto lg:max-w-[75%] overflow-hidden flex justify-between items-center py-5 px-5 md:px-10">
-      <img src={assets.logo} alt="Logo" className="h-5 lg:h-10" />
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="h-5 lg:h-10" />
+      </Link>
 
-      <ul className="hidden md:flex items-center gap-5 text-lg text-[#49557e]">
+      <ul className="hidden md:flex items-center gap-5 text-lg text-white">
         <Link
           to="/"
           className={`cursor-pointer ${
@@ -42,9 +44,14 @@ const Navbar = ({ setShowLogin }) => {
       <div className="flex items-center gap-3 ml-5 lg:ml-0 lg:gap-10">
         <img src={assets.search_icon} alt="Search" className="w-6 h-6" />
 
-        <img src={assets.basket_icon} alt="Cart" className="w-6 h-6" />
+        <Link to="/cart">
+          <img src={assets.basket_icon} alt="Cart" className="w-6 h-6" />
+        </Link>
 
-        <button onClick={()=>setShowLogin(true)} className="bg-transparent text-sm lg:text-lg text-white font-bold border border-tomato lg:py-2 px-5 lg:px-7 rounded-full hover:bg-[#fff4f2] hover:text-black cursor-pointer">
+        <button
+          onClick={() => setShowLogin(true)}
+          className="bg-transparent text-sm lg:text-lg text-white font-bold border border-tomato lg:py-2 px-5 lg:px-7 rounded-full hover:bg-[#fff4f2] hover:text-black cursor-pointer"
+        >
           sign in
         </button>
       </div>
