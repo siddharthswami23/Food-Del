76481b7b8c14d7bcc2e7031cdea1ca367b0f0e23
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2025-05-29 13:49:31 +0530

    order model added

---
+++ backend/models/OrderModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    items: {
+        type: Array,
+        required: true
+    },
+    address: {
+        type: Object,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    status: {
+        type: String,
+        default: "Food Processing"
+    },
+    payment: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const OrderModel = mongoose.models.order || mongoose.model("order", orderSchema);
+export default OrderModel;
